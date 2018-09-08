dec6045848c36444c3d1a11bee123da40a57d8b6
fauziridi172 <39006681+fauziridi172@users.noreply.github.com>
2018-09-08 18:36:23 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs");
+
+const client = new Discord.Client();
+const config = require("./config.json");
+// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
+client.config = config;
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
