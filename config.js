261cdfe1fd9169b98493b86c2192c0701c7523a9
fauziridi172 <39006681+fauziridi172@users.noreply.github.com>
2018-09-08 18:50:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.PREFIX = '/';
 
 exports.GOOGLE_API_KEY = 'AIzaSyB55tLFdHncg2ZmAhki_AaeYAsUEsRfkZA';
 
-exports.TOKEN = 'NDU5NzM1MTU0ODE1MTM5ODQw.Dg7DLw.8Q7bGiBDFejZJ3glQGhpqd6YjE8';
+exports.TOKEN = 'NDQyMzEyMTAzOTE1NjgzODQy.DnVA3w.rCc3dfOdoHdhMG7AZKISOBMEfb4';
